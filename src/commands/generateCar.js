bff93dfb5f0da64a5fb3ed66eea453439bb8a011
the-simian <harlinjesse@gmail.com>
2022-08-16 13:54:34 -0500

    increment version, remove log, review code

--- src/commands/generateCar.js
+++ src/commands/generateCar.js
@@ -1,11 +1,10 @@
-import { buildCar } from '../lib/car.js';
-import fs from 'fs';
+import { buildCar } from "../lib/car.js";
+import fs from "fs";
 
 export const generateCar = async (path) => {
-  console.log('hello', path);
   return buildCar(path);
 };
 
-export const writeFileLocally = async (car, outPath = 'output.car') => {
-  fs.writeFileSync(outPath, car, { encoding: 'binary' });
+export const writeFileLocally = async (car, outPath = "output.car") => {
+  fs.writeFileSync(outPath, car, { encoding: "binary" });
 };
