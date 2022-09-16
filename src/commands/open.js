6aff365fbd174f44ecb85349ffd58480540a7b26
the-simian <harlinjesse@gmail.com>
2022-09-16 00:06:27 -0500

    chore: unhappy path on open

--- src/commands/open.js
+++ src/commands/open.js
@@ -1,6 +1,8 @@
+import * as API from '@ucanto/interface'
 import open from 'open'
 
 import { OPEN_WITH_SERVICE_URL } from '../settings.js'
+import { isCID } from '../validation.js'
 
 /**
  * @typedef {{cid?:string}} Cid
@@ -13,13 +15,40 @@ import { OPEN_WITH_SERVICE_URL } from '../settings.js'
  * @returns {Promise<void>}
  */
 const exe = async ({ cid }) => {
-  open(`${OPEN_WITH_SERVICE_URL}${cid}`)
+  try {
+    open(`${OPEN_WITH_SERVICE_URL}${cid}`)
+  } catch (err) {
+    throw `Could not open ${cid}: ${err}`
+  }
+}
+
+/**
+ * @type {import('yargs').CommandBuilder} yargs
+ * @returns {import('yargs').Argv<{}>}
+ */
+const build = (yargs) => yargs.check(checkTarget)
+
+/**
+ * @param {CidArgs} argv
+ */
+const checkTarget = ({ cid }) => {
+  if (!cid) {
+    throw new Error(`No valid cid or item was provided to open.`)
+  }
+
+  const _cid = cid.split('/')[0]
+
+  try {
+    return isCID(_cid)
+  } catch (err) {
+    throw new Error(`${_cid} is probably not a valid CID: \n${err}`)
+  }
 }
 
 const openCmd = {
   cmd: 'open <cid>',
   description: 'Open a CID in your browser on w3s.link',
-  build: {},
+  build,
   exe,
   exampleIn: '$0 open bafy...',
   exampleOut: `# opens ${OPEN_WITH_SERVICE_URL}/bafy... in your browser`,
