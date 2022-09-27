642f656f95f3e58b3bce1667cd236a89e40c4efd
the-simian <harlinjesse@gmail.com>
2022-09-27 16:31:32 -0500

    feat: adds verbose flag

--- src/commands/list.js
+++ src/commands/list.js
@@ -4,16 +4,21 @@ import client from '../client.js'
 import { hasID } from '../validation.js'
 
 /**
+ * @typedef {{verbose?:boolean}} List
+ * @typedef {import('yargs').Arguments<Upload>} ListArgs
+ */
+
+/**
  *
  * @param {Array<any>} list
+ * @param {boolean} verbose
  * @returns {Array<any>}
  */
-const listToTable = (list) =>
+const listToTable = (list, verbose = false) =>
   list.map((li) => {
     const at = new Intl.DateTimeFormat('en-US').format(li.uploadedAt)
     let out = `${at.toLocaleString()}  ${li.rootContentCID}`
 
-    const verbose = false
     if (verbose) {
       out = `${at.toLocaleString()}  ${li.rootContentCID} \t ${li.carCID}`
     }
@@ -36,26 +41,38 @@ const listToTable = (list) =>
 /**
  *
  * @param {PagedListResponse} listResponse
+ *  @param {boolean} verbose
  * @returns {string}
  */
-const formatOutput = (listResponse) => {
+const formatOutput = (listResponse, verbose = false) => {
   const list = listResponse?.results || []
   const space = `\t\t\t\t\t\t\t`
   let headers = `Date       Root CID `
   let divider = `---------  -------  `
 
-  const verbose = false
   if (verbose) {
     headers = `Date       Root CID ${space} CAR CID`
     divider = `---------  -------  ${space} -------`
   }
 
   const footer = `\nCount: ${listResponse?.count}`
-  const output = [headers, divider, listToTable(list).join('\n'), footer]
+  const output = [
+    headers,
+    divider,
+    listToTable(list, verbose).join('\n'),
+    footer,
+  ]
   return `\n${output.join('\n')}`
 }
 
-const exe = async () => {
+/**
+ * @async
+ * @param {ListArgs} argv
+ * @returns {Promise<void>}
+ */
+const exe = async (argv) => {
+  const verbose = argv.verbose
+
   const view = ora()
   /**
    * @type any
@@ -68,7 +85,7 @@ const exe = async () => {
   if (!listResponse?.results?.length) {
     view.info(`You don't seem to have any uploads yet!`)
   } else {
-    const formattedOutput = formatOutput(listResponse)
+    const formattedOutput = formatOutput(listResponse, verbose)
     view.succeed(formattedOutput)
   }
 }
@@ -76,7 +93,15 @@ const exe = async () => {
 /**
  * @type {import('yargs').CommandBuilder} yargs
  */
-const build = (yargs) => yargs.check(() => hasID())
+const build = (yargs) =>
+  yargs
+    .check(() => hasID())
+    .option('verbose', {
+      type: 'boolean',
+      alias: 'verbose',
+      showInHelp: true,
+      describe: 'Show more columns in the list, such as the Uploaded CAR CID',
+    })
 
 const list = {
   cmd: 'list',
