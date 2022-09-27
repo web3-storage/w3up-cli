7d16435b7afe9122f9760951548c9038984c3df9
the-simian <harlinjesse@gmail.com>
2022-09-27 12:09:42 -0500

    feat: ignore fialure log, add better mapper in the list command

--- src/commands/list.js
+++ src/commands/list.js
@@ -3,20 +3,44 @@ import ora, { oraPromise } from 'ora'
 import client from '../client.js'
 import { hasID } from '../validation.js'
 
+/**
+ *
+ * @param {Array<any>} list
+ * @returns {Array<any>}
+ */
+const listToTable = (list) =>
+  list.map((li) => {
+    const at = new Intl.DateTimeFormat('en-US').format(li.uploadedAt)
+    return `${at.toLocaleString()}  ${li.rootCarCID} \t ${li.transportCarCID}`
+  })
+
+/**
+ *
+ * @param {*} list
+ * @returns {string}
+ */
+const formatOutput = (list) => {
+  const space = `\t\t\t\t\t\t\t`
+  const headers = `Date       RootCid ${space} Transport CID`
+  const divider = `---------  ------- ${space} -------------`
+  return `\n${headers}\n${divider}\n${listToTable(list).join('\n')}`
+}
+
 const exe = async () => {
   const view = ora()
   /**
    * @type any
    */
-  const list = await oraPromise(client.list(), {
+  const listResponse = await oraPromise(client.list(), {
     text: `Listing Uploads...`,
     spinner: 'line',
   })
 
-  if (!list.length) {
+  if (!listResponse.length) {
     view.info(`You don't seem to have any uploads yet!`)
   } else {
-    view.succeed(`CIDs:\n${list.join('\n')}`)
+    const formattedOutput = formatOutput(listResponse)
+    view.succeed()
   }
 }
 
